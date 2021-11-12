af5f0fcd503d55f72790129c4b1d1d60a17150f9
Achyuth Naidu <ashwath.bly@gamil.com>
2021-11-12 08:45:44 +0530

    files committed

---
+++ forloop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for file in `ls *.txt`
+
+do
+ foldername=`echo $file | awk -F. '{print $1}'`
+ mkdir $foldername
+ cp $file $forldername
+ echo  copied  $file  into $foldername 
+done
